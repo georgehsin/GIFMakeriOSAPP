cead0e1d0083a9fa568b6638ef022fb785446536
George Hsin <georgehsin@ucla.edu>
2016-11-14 20:31:19 -0800

    Initial Commit

---
+++ iOSHax2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iOSHax2
+//
+//  Created by George Hsin on 11/14/16.
+//  Copyright © 2016 George Hsin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
